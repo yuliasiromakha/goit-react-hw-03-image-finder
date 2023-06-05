4c8b67862c2c6eaabfa307c27e419f150d08c84e
yuliasiromakha <93142108+yuliasiromakha@users.noreply.github.com>
2023-06-05 19:57:39 +0300

    modal

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,27 +1,7 @@
-// import React from 'react';
-
-// class ImageGalleryItem extends React.Component {
-//   handleClick = () => {
-//     const { imageUrl, altText, toggleModal } = this.props;
-//     toggleModal(imageUrl, altText);
-//   };
-
-//   render() {
-//     const { imageUrl, altText } = this.props;
-//     return (
-//       <li className="gallery-item" onClick={this.handleClick}>
-//         <img src={imageUrl} alt={altText} width={300} height={200}/>
-//       </li>
-//     );
-//   }
-// }
-
-// export default ImageGalleryItem;
-
 const ImageGalleryItem = ({ imageUrl, altText, toggleModal }) => {
   return (
-    <li className="gallery-item">
-      <img src={imageUrl} alt={altText} width={300} height={200} onClick={toggleModal} />
+    <li className="gallery-item" onClick={toggleModal}>
+      <img src={imageUrl} alt={altText} width={300} height={200}  />
     </li>
   );
 };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,39 +1,3 @@
-// import React from 'react';
-// import * as basicLightbox from 'basiclightbox';
-
-// class Modal extends React.Component {
-//   modalRef = React.createRef();
-
-//   componentDidMount() {
-//     this.showModal();
-//   }
-
-//   showModal() {
-//     const { imageUrl, altText, toggleModal } = this.props;
-
-//     const modalContent = `
-//       <div className="overlay">
-//         <div className="modal">
-//           <img src="${imageUrl}" alt="${altText}" />
-//         </div>
-//       </div>
-//     `;
-
-//     const lightbox = basicLightbox.create(modalContent, {
-//       onClose: toggleModal,
-//     });
-
-//     lightbox.show();
-//   }
-
-//   render() {
-//     return null;
-//   }
-// }
-
-// export default Modal;
-
-
 import * as basicLightbox from 'basiclightbox'
 
 const Modal = ((largeImageUrl, tags) => {
@@ -41,7 +5,7 @@ const Modal = ((largeImageUrl, tags) => {
     basicLightbox.create(`
         <div className="overlay">
             <div className="modal">
-              <p>modal</p>
+                <img src=${largeImageUrl} alt="${tags}" />
             </div>
         </div>
 `)
