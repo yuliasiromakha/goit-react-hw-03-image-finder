e33bb890dd2bd875bf3f85a69dff9f97718009a8
yuliasiromakha <93142108+yuliasiromakha@users.noreply.github.com>
2023-06-25 21:19:52 +0300

    removed class

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,32 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import Modal from 'components/Modal/Modal';
 import './ImageGallery.css';
 
-class ImageGallery extends Component {
-  render() {
-    const { images, showModal, modalImageURL, toggleModal } = this.props;
+const ImageGallery = ({images, showModal, modalImageURL, toggleModal}) => {
 
-    return (
-      <div>
-        <>
-          <ul className="gallery">
-            {images.map((item) => (
-              <ImageGalleryItem
-                toggleModal={toggleModal}
-                key={item.id}
-                imageUrl={item.webformatURL}
-                altText={item.tags}
-                largeImageURL={item.largeImageURL}
-              />
-            ))}
-          </ul>
-        </>
+  return (
+    <div>
+      <>
+        <ul className="gallery">
+          {images.map((item) => (
+            <ImageGalleryItem
+              toggleModal={toggleModal}
+              key={item.id}
+              imageUrl={item.webformatURL}
+              altText={item.tags}
+              largeImageURL={item.largeImageURL}
+            />
+          ))}
+        </ul>
+      </>
 
-        {showModal && <Modal largeImageURL={modalImageURL} handleModalClose={toggleModal} />}
-      </div>
-    );
-  }
+      {showModal && <Modal largeImageURL={modalImageURL} handleModalClose={toggleModal} />}
+    </div>
+  );
+  
 }
-
 export default ImageGallery;
